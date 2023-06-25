11ca32c61f87bdfe1844d23efafd4f71fd196000
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-06-25 18:31:56 +0600

    [ADDED] templatetag and base html added

--- src/book_store/urls.py
+++ src/book_store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-    
+    path('', Index.as_view(), name='homepage'),
+    path('store/', store , name='store'),
 ]
--- src/book_store/views.py
+++ src/book_store/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render , redirect , HttpResponseRedirect
+from .models import Book
+from django.views import View
+
+
+# Create your views here.
+class Index(View):
+
+    def get(self , request):
+        return HttpResponseRedirect(f'/store{request.get_full_path()[1:]}')
+
+    def post(self , request):
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product]  = quantity-1
+                else:
+                    cart[product]  = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        return redirect('homepage')
+
+
+def store(request):
+    cart = request.session.get('cart')
+    if not cart:
+        request.session['cart'] = {}
+    products = Book.get_all_books()
+
+    data = {}
+    data['products'] = products
+
+    return render(request, 'index.html', data)
